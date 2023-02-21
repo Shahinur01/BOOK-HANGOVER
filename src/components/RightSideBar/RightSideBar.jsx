1261e28bc70e5404541130ca0a962628313bf61e
Shahinur01 <shahinurislam903@gmail.com>
2023-02-21 09:09:47 +0600

    update file

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './RightSideBar.css';
 
 const RightSideBar = (props) => {
-    let { cart,setCart } = props;
-    // const [data,setData] =useState(cart);
-    
+    let { cart, setCart } = props;
+    // const [handleAgain,setHandleAgain] = useState([]);
+
+
+    const handleChooseAgain = () => {
+        setCart([]);
+    }
 
     const handleDelete = (id) => {
         const deleteCart = cart.filter(c => c.id !== id);
+        // const deleteCart = cart.splice(cart.IndexOf(id),1);
         setCart(deleteCart)
         console.log(deleteCart);
     }
@@ -15,14 +20,16 @@ const RightSideBar = (props) => {
     return (
         <div className="RightSideBar">
             <h3>Selected Books:{cart.length}</h3>
+            <div className="btn btn-primary">Choose for 1 me</div> &nbsp;
+            <div onClick={handleChooseAgain} className="btn btn-success">Choose Again</div>
 
             {
-                cart.map(({ image,id }, index) => {
+                cart.map(({ image, id }, index) => {
                     return (
                         <div key={index}>
                             <div className="d-flex my-3">
                                 <img src={image} alt="" /> <br />
-                                <button onClick={() =>handleDelete(id)}><i className='bx bxs-trash'></i></button>
+                                <button onClick={() => handleDelete(id)}><i className='bx bxs-trash'></i></button>
                             </div>
                         </div>
                     )
